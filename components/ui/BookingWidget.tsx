f93daa120e8455fc44c19f0e3d169d4aabe4121c
magoyagardi <yagominardi2005@gmail.com>
2026-05-21 23:01:03 -0300

    html structure of the booking- with calendar (react-day-picker) integration

--- components/ui/BookingWidget.tsx
+++ components/ui/BookingWidget.tsx
@@ -1,11 +1,63 @@
 import styles from './BookingWidget.module.css'
+import { DayPicker } from  'react-day-picker'
+import { useState } from 'react'
+import 'react-day-picker/dist/style.css'
 
 export default function BookingWidget () {
-
+const [selected, setSelected] = useState(['']);
 
     return (
-        <aside className={styles.widgetWrap}>
-            <p>this is the widget - start your booking</p>
-        </aside>
+        <article className={styles.widgetWrap}>
+            <div>
+                <h1>Selecciona Destino & Fecha</h1>
+
+                <h2>Elije las fechas en el calendario y
+                    <br />
+                    encuentra que hay disponible</h2>
+                <hr />
+            </div>
+            <form className={styles.bookingForm}>
+                <label>
+                    Destino
+                    <select name="" id="">
+                        <option value="place">place</option>
+                        <option value="place">place 1</option>
+                        <option value="place">place 2</option>                        
+                    </select>
+                </label>
+                <label>
+                    Fecha inicial
+                    <input type="date" name="" id="" />    
+                </label>
+                <label>
+                    Fecha final
+                    <input type="date" name="" id="" />    
+                </label>
+                {/* calendario */}
+                <DayPicker
+                    mode='single'
+                    selected={selected}
+                    onSelect={setSelected}
+                ></DayPicker>
+
+
+                <label>
+                    Habitaciones
+                    <input type="number" name="" id="" />    
+                </label>
+                <label>
+                    Adultos
+                    <input type="number" name="" id="" />    
+                </label>
+                <label>
+                    Ninios
+                    <input type="number" name="" id="" />    
+                </label>
+                <label>
+                    Codigo Promocional
+                    <input type="number" name="" id="" />    
+                </label>
+            </form>
+        </article>
     )
 }
